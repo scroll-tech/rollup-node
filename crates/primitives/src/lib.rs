2c18c7700a9d9a081c3d5529e89a2ab3a55b337f
Gregory Edison <gregory.edison1993@gmail.com>
2025-03-05 12:51:22 +0100

    feat(watcher): add error types

--- crates/primitives/src/lib.rs
+++ crates/primitives/src/lib.rs
@@ -1,4 +1,10 @@
 //! Primitive types for the Rollup Node.
 
-mod block;
 pub use block::BlockInfo;
+mod block;
+
+pub use batch::{BatchInput, BatchInputBuilder, BatchInputV1, BatchInputV2};
+mod batch;
+
+pub use transaction::L1MessageWithBlockNumber;
+mod transaction;
