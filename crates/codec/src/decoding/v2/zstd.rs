e4e1b14579db9eea6a6056c14ee122fcecf46858
Gregory Edison <gregory.edison1993@gmail.com>
2025-03-20 12:25:57 +0100

    docs: add comments

--- crates/codec/src/decoding/v2/zstd.rs
+++ crates/codec/src/decoding/v2/zstd.rs
@@ -12,10 +12,18 @@ const ZSTD_MAGIC_NUMBER: [u8; 4] = [0x28, 0xb5, 0x2f, 0xfd];
 pub fn decompress_blob_data(data: &[u8]) -> Vec<u8> {
     let mut header_data = ZSTD_MAGIC_NUMBER.to_vec();
     header_data.extend_from_slice(data);
-    let mut output = Vec::with_capacity(USABLE_BYTES_PER_BLOB);
-    let mut decompressor = Decoder::new(header_data.as_slice()).unwrap();
-    let mut dst = vec![0u8; 8192];
-    while let Ok(size) = decompressor.read(&mut dst) {
+
+    // init decoder and owned output data.
+    let mut decoder = Decoder::new(header_data.as_slice()).unwrap();
+    // heuristic: use data length as the allocated output capacity.
+    let mut output = Vec::with_capacity(header_data.len());
+
+    // use an output buffer with the recommended output size.
+    let buffer_size = Decoder::<'static, &[u8]>::recommended_output_size();
+    let mut dst = vec![0u8; buffer_size];
+
+    while let Ok(size) = decoder.read(&mut dst) {
+        // break if we read 0 bytes.
         if size == 0 {
             break
         }
