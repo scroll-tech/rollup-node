6f979604ab4979d9227da525455b0870cc76d6f7
greg <82421016+greged93@users.noreply.github.com>
2025-03-24 10:05:44 +0100

    feat: codec (#38)
    * feat: codec
    
    * feat: check buf length before using from_be_bytes macro
    
    * fix: lints
    
    * chore: remove the `std` feature
    
    * docs: add comments
    
    * chore: cleaning
    
    * fix: lints
    
    * fix: lints
    
    * fix: lints
    
    * chore: remove duplicate abis
    
    * fix: zepter

---
+++ crates/codec/src/decoding/transaction.rs
@@ -0,0 +1,39 @@
+use alloy_primitives::{Bytes, bytes::Buf};
+use alloy_rlp::Header;
+
+/// A RLP encoded transaction.
+#[derive(Debug)]
+pub struct Transaction(pub Bytes);
+
+impl Transaction {
+    /// Tries to read from the input buffer into the [`Transaction`]. Returns [`None`] if it can't
+    /// read the RLP header from the buffer or the buffer length does not cover all the expected
+    /// payload data length.
+    pub(super) fn try_from_buf(buf: &mut &[u8]) -> Option<Self> {
+        // clone the buffer in order to avoid advancing it.
+        #[allow(suspicious_double_ref_op)]
+        let header = Header::decode(&mut buf.clone()).ok()?;
+        let finish = |buf: &mut &[u8], offset: usize| {
+            if buf.remaining() < offset {
+                return None
+            }
+
+            // copy the transaction bytes and advance the buffer.
+            let tx = Transaction(buf[0..offset].to_vec().into());
+            buf.advance(offset);
+            Some(tx)
+        };
+
+        if header.list {
+            // legacy tx.
+            finish(buf, header.length_with_payload())
+        } else {
+            // typed transaction.
+            let mut tx_decode = *buf;
+            let _ = tx_decode.get_u8();
+            let header = Header::decode(&mut tx_decode).ok()?;
+
+            finish(buf, header.length_with_payload() + 1)
+        }
+    }
+}
