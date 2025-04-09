38ea6cf9054576ed4ba3c220d989b99e0d92a91a
Gregory Edison <gregory.edison1993@gmail.com>
2025-04-09 09:34:49 +0200

    fix: revert constant changes

--- bin/rollup/src/constants.rs
+++ bin/rollup/src/constants.rs
@@ -1,5 +1,5 @@
 /// The block number at which to start the L1 watcher.
-pub const WATCHER_START_BLOCK_NUMBER: u64 = 8000374;
+pub const WATCHER_START_BLOCK_NUMBER: u64 = 18318215;
 
 /// The size of the blob cache for the provider.
 pub const PROVIDER_BLOB_CACHE_SIZE: usize = 100;
