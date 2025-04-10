03257da134588196b50aca6282b4c93b9c51e686
Gregory Edison <gregory.edison1993@gmail.com>
2025-04-10 18:26:39 +0200

    fix: lints

--- crates/providers/src/block.rs
+++ crates/providers/src/block.rs
@@ -4,7 +4,7 @@ use alloy_eips::BlockId;
 use scroll_alloy_rpc_types_engine::BlockDataHint;
 use scroll_db::{DatabaseConnectionProvider, DatabaseError, DatabaseOperations};
 
-/// Trait implementors can return block data.
+/// Trait implementers can return block data.
 #[async_trait::async_trait]
 pub trait BlockDataProvider {
     /// The error type for the provider.
