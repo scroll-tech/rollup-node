7ce13202bb8a118a10aec7d59afdfb7eb7b4ed80
Gregory Edison <gregory.edison1993@gmail.com>
2025-04-15 19:32:41 +0200

    feat: compute rolling hash in indexer

--- crates/indexer/src/lib.rs
+++ crates/indexer/src/lib.rs
@@ -1,9 +1,12 @@
 //! A library responsible for indexing data relevant to the L1.
-use alloy_primitives::B256;
+
+use alloy_primitives::{b256, keccak256, B256};
 use futures::Stream;
-use rollup_node_primitives::{BatchCommitData, L1MessageWithBlockNumber};
+use rollup_node_primitives::{BatchCommitData, L1MessageEnvelope};
 use rollup_node_watcher::L1Notification;
-use scroll_db::{Database, DatabaseOperations};
+use scroll_alloy_consensus::TxL1Message;
+use scroll_alloy_hardforks::{ScrollHardfork, ScrollHardforks};
+use scroll_db::{Database, DatabaseError, DatabaseOperations};
 use std::{
     collections::VecDeque,
     pin::Pin,
@@ -20,19 +23,24 @@ pub use event::IndexerEvent;
 mod error;
 pub use error::IndexerError;
 
+const L1_MESSAGE_QUEUE_HASH_MASK: B256 =
+    b256!("ffffffffffffffffffffffffffffffffffffffffffffffffffffffff00000000");
+
 /// The indexer is responsible for indexing data relevant to the L1.
 #[derive(Debug)]
-pub struct Indexer {
+pub struct Indexer<ChainSpec> {
     /// A reference to the database used to persist the indexed data.
     database: Arc<Database>,
     /// A queue of pending futures.
     pending_futures: VecDeque<IndexerFuture>,
+    /// The chain specification for the indexer.
+    chain_spec: Arc<ChainSpec>,
 }
 
-impl Indexer {
+impl<ChainSpec: ScrollHardforks + Send + Sync + 'static> Indexer<ChainSpec> {
     /// Creates a new indexer with the given [`Database`].
-    pub fn new(database: Arc<Database>) -> Self {
-        Self { database, pending_futures: Default::default() }
+    pub fn new(database: Arc<Database>, chain_spec: Arc<ChainSpec>) -> Self {
+        Self { database, pending_futures: Default::default(), chain_spec }
     }
 
     /// Handles an event from the L1.
@@ -47,9 +55,15 @@ impl Indexer {
                 L1Notification::BatchCommit(batch) => IndexerFuture::HandleBatchCommit(Box::pin(
                     Self::handle_batch_commit(self.database.clone(), batch),
                 )),
-                L1Notification::L1Message(l1_message) => IndexerFuture::HandleL1Message(Box::pin(
-                    Self::handle_l1_message(self.database.clone(), l1_message),
-                )),
+                L1Notification::L1Message { message, block_number, block_timestamp } => {
+                    IndexerFuture::HandleL1Message(Box::pin(Self::handle_l1_message(
+                        self.database.clone(),
+                        self.chain_spec.clone(),
+                        message,
+                        block_number,
+                        block_timestamp,
+                    )))
+                }
                 L1Notification::BatchFinalization { hash, block_number } => {
                     IndexerFuture::HandleBatchFinalization(Box::pin(
                         Self::handle_batch_finalization(self.database.clone(), hash, block_number),
@@ -81,9 +95,33 @@ impl Indexer {
     /// Handles an L1 message by inserting it into the database.
     async fn handle_l1_message(
         database: Arc<Database>,
-        l1_message: L1MessageWithBlockNumber,
+        chain_spec: Arc<ChainSpec>,
+        l1_message: TxL1Message,
+        block_number: u64,
+        block_timestamp: u64,
     ) -> Result<IndexerEvent, IndexerError> {
-        let event = IndexerEvent::L1MessageIndexed(l1_message.transaction.queue_index);
+        let event = IndexerEvent::L1MessageIndexed(l1_message.queue_index);
+
+        // TODO(greg): update to Euclid.
+        let queue_hash = if chain_spec
+            .scroll_fork_activation(ScrollHardfork::DarwinV2)
+            .active_at_timestamp_or_number(block_timestamp, block_number)
+        {
+            let index = l1_message.queue_index - 1;
+            let prev_queue_hash = database
+                .get_l1_message(index)
+                .await?
+                .map(|m| m.queue_hash)
+                .ok_or(DatabaseError::L1MessageNotFound(index))?;
+
+            let mut input = prev_queue_hash.to_vec();
+            input.append(&mut l1_message.tx_hash().to_vec());
+            keccak256(input) & L1_MESSAGE_QUEUE_HASH_MASK
+        } else {
+            B256::ZERO
+        };
+
+        let l1_message = L1MessageEnvelope::new(l1_message, block_number, queue_hash);
         database.insert_l1_message(l1_message).await?;
         Ok(event)
     }
@@ -110,7 +148,7 @@ impl Indexer {
     }
 }
 
-impl Stream for Indexer {
+impl<ChainSpec: ScrollHardforks + 'static> Stream for Indexer<ChainSpec> {
     type Item = Result<IndexerEvent, IndexerError>;
 
     fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
@@ -132,16 +170,18 @@ impl Stream for Indexer {
 #[cfg(test)]
 mod test {
     use super::*;
+    use alloy_primitives::{address, bytes, U256};
 
     use arbitrary::{Arbitrary, Unstructured};
     use futures::StreamExt;
     use rand::Rng;
+    use reth_scroll_chainspec::{ScrollChainSpec, SCROLL_MAINNET};
     use rollup_node_primitives::BatchCommitData;
     use scroll_db::test_utils::setup_test_db;
 
-    async fn setup_test_indexer() -> (Indexer, Arc<Database>) {
+    async fn setup_test_indexer() -> (Indexer<ScrollChainSpec>, Arc<Database>) {
         let db = Arc::new(setup_test_db().await);
-        (Indexer::new(db.clone()), db)
+        (Indexer::new(db.clone(), SCROLL_MAINNET.clone()), db)
     }
 
     #[tokio::test]
@@ -174,15 +214,61 @@ mod test {
         rand::rng().fill(bytes.as_mut_slice());
         let mut u = Unstructured::new(&bytes);
 
-        let l1_message = L1MessageWithBlockNumber::arbitrary(&mut u).unwrap();
-        indexer.handle_l1_notification(L1Notification::L1Message(l1_message.clone()));
+        let message = TxL1Message {
+            queue_index: i64::arbitrary(&mut u).unwrap().abs() as u64,
+            ..Arbitrary::arbitrary(&mut u).unwrap()
+        };
+        let block_number = u64::arbitrary(&mut u).unwrap();
+        indexer.handle_l1_notification(L1Notification::L1Message {
+            message: message.clone(),
+            block_number,
+            block_timestamp: 0,
+        });
 
         let _ = indexer.next().await;
 
-        let l1_message_result =
-            db.get_l1_message(l1_message.transaction.queue_index).await.unwrap().unwrap();
+        let l1_message_result = db.get_l1_message(message.queue_index).await.unwrap().unwrap();
+        let l1_message = L1MessageEnvelope::new(message, block_number, B256::ZERO);
 
         assert_eq!(l1_message, l1_message_result);
+    }
+
+    #[tokio::test]
+    async fn test_l1_message_hash_queue() {
+        // Instantiate indexer and db
+        let (mut indexer, db) = setup_test_indexer().await;
+
+        // insert the previous L1 message in database.
+        indexer.handle_l1_notification(L1Notification::L1Message {
+            message: TxL1Message { queue_index: 1062109, ..Default::default() },
+            block_number: 1475588,
+            block_timestamp: 172526400,
+        });
+        let _ = indexer.next().await.unwrap().unwrap();
+
+        // <https://sepolia.scrollscan.com/tx/0xd80cd61ac5d8665919da19128cc8c16d3647e1e2e278b931769e986d01c6b910>
+        let message = TxL1Message {
+            queue_index: 1062110,
+            gas_limit: 168000,
+            to: address!("Ba50f5340FB9F3Bd074bD638c9BE13eCB36E603d"),
+            value: U256::ZERO,
+            sender: address!("61d8d3E7F7c656493d1d76aAA1a836CEdfCBc27b"),
+            input: bytes!("8ef1332e000000000000000000000000323522a8de3cddeddbb67094eecaebc2436d6996000000000000000000000000323522a8de3cddeddbb67094eecaebc2436d699600000000000000000000000000000000000000000000000000038d7ea4c6800000000000000000000000000000000000000000000000000000000000001034de00000000000000000000000000000000000000000000000000000000000000a00000000000000000000000000000000000000000000000000000000000000000"),
+        };
+        indexer.handle_l1_notification(L1Notification::L1Message {
+            message: message.clone(),
+            block_number: 14755883,
+            block_timestamp: 1725264000,
+        });
+
+        let _ = indexer.next().await.unwrap().unwrap();
+
+        let l1_message_result = db.get_l1_message(message.queue_index).await.unwrap().unwrap();
+
+        assert_eq!(
+            b256!("5e48ae1092c7f912849b9935f4e66870d2034b24fb2016f506e6754900000000"),
+            l1_message_result.queue_hash
+        );
     }
 
     #[tokio::test]
@@ -214,28 +300,43 @@ mod test {
         indexer.handle_l1_notification(L1Notification::BatchCommit(batch_commit_block_30.clone()));
 
         // Generate 3 random L1 messages and set their block numbers
-        let mut l1_message_block_1 = L1MessageWithBlockNumber::arbitrary(&mut u).unwrap();
+        let mut l1_message_block_1 =
+            L1MessageEnvelope { queue_hash: B256::ZERO, ..Arbitrary::arbitrary(&mut u).unwrap() };
         l1_message_block_1.block_number = 1;
         let l1_message_block_1 = l1_message_block_1;
 
-        let mut l1_message_block_20 = L1MessageWithBlockNumber::arbitrary(&mut u).unwrap();
+        let mut l1_message_block_20 =
+            L1MessageEnvelope { queue_hash: B256::ZERO, ..Arbitrary::arbitrary(&mut u).unwrap() };
         l1_message_block_20.block_number = 20;
         let l1_message_block_20 = l1_message_block_20;
 
-        let mut l1_message_block_30 = L1MessageWithBlockNumber::arbitrary(&mut u).unwrap();
+        let mut l1_message_block_30 =
+            L1MessageEnvelope { queue_hash: B256::ZERO, ..Arbitrary::arbitrary(&mut u).unwrap() };
         l1_message_block_30.block_number = 30;
         let l1_message_block_30 = l1_message_block_30;
 
         // Index L1 messages
-        indexer.handle_l1_notification(L1Notification::L1Message(l1_message_block_1.clone()));
-        indexer.handle_l1_notification(L1Notification::L1Message(l1_message_block_20.clone()));
-        indexer.handle_l1_notification(L1Notification::L1Message(l1_message_block_30.clone()));
+        indexer.handle_l1_notification(L1Notification::L1Message {
+            message: l1_message_block_1.clone().transaction,
+            block_number: l1_message_block_1.clone().block_number,
+            block_timestamp: 0,
+        });
+        indexer.handle_l1_notification(L1Notification::L1Message {
+            message: l1_message_block_20.clone().transaction,
+            block_number: l1_message_block_20.clone().block_number,
+            block_timestamp: 0,
+        });
+        indexer.handle_l1_notification(L1Notification::L1Message {
+            message: l1_message_block_30.clone().transaction,
+            block_number: l1_message_block_30.clone().block_number,
+            block_timestamp: 0,
+        });
 
         // Reorg at block 20
         indexer.handle_l1_notification(L1Notification::Reorg(20));
 
         for _ in 0..7 {
-            indexer.next().await;
+            indexer.next().await.unwrap().unwrap();
         }
 
         // Check that the batch input at block 30 is deleted
