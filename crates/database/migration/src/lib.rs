94a2bb3ac74a284f0e17d0acb612534e78bfcfa0
Gregory Edison <gregory.edison1993@gmail.com>
2025-06-04 16:26:22 +0200

    fix: migration

--- crates/database/migration/src/lib.rs
+++ crates/database/migration/src/lib.rs
@@ -2,8 +2,8 @@ pub use sea_orm_migration::prelude::*;
 
 mod m20220101_000001_create_batch_commit_table;
 mod m20250304_125946_add_l1_msg_table;
-mod m20250408_132123_add_block_data_table;
-mod m20250408_150338_add_header_metadata;
+mod m20250408_132123_add_header_metadata;
+mod m20250408_150338_load_header_metadata;
 mod m20250411_072004_add_l2_block;
 mod migration_info;
 pub use migration_info::{MigrationInfo, ScrollMainnetMigrationInfo, ScrollSepoliaMigrationInfo};
@@ -16,8 +16,8 @@ impl<MI: MigrationInfo + Send + Sync + 'static> MigratorTrait for Migrator<MI> {
         vec![
             Box::new(m20220101_000001_create_batch_commit_table::Migration),
             Box::new(m20250304_125946_add_l1_msg_table::Migration),
-            Box::new(m20250408_132123_add_block_data_table::Migration),
-            Box::new(m20250408_150338_add_header_metadata::Migration::<MI>(Default::default())),
+            Box::new(m20250408_132123_add_header_metadata::Migration),
+            Box::new(m20250408_150338_load_header_metadata::Migration::<MI>(Default::default())),
             Box::new(m20250411_072004_add_l2_block::Migration),
         ]
     }
