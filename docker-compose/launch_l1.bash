3f261455610ef75f28ed95c65204c3182de497a1
Morty <yiweichi1@gmail.com>
2025-07-09 01:35:19 +0800

    chore: move docker compose command to separate files

---
+++ docker-compose/launch_l1.bash
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -e
+
+EXTRA_PARAMS=""
+if [ "${FORK_BLOCK_NUMBER}" != "" ]; then
+  EXTRA_PARAMS="--fork-block-number ${FORK_BLOCK_NUMBER}"
+fi
+
+if [ "${ENV:-}" = "sepolia" ]; then
+  exec anvil --fork-url http://l1reth-rpc.sepolia.scroll.tech:8545 --chain-id 11155111 --host 0.0.0.0 --block-time 12 $EXTRA_PARAMS
+elif [ "${ENV:-}" = "mainnet" ]; then
+  exec anvil --fork-url http://l1geth-rpc.mainnet.scroll.tech:8545/l1 --chain-id 1 --host 0.0.0.0 --block-time 12 $EXTRA_PARAMS
+fi 
