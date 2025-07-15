c34d2e4dde1ebbe57adb47e26c44ebce37d225d1
Gregory Edison <gregory.edison1993@gmail.com>
2025-07-15 09:21:40 +0200

    fix: remove dev from launch_l1.bash

--- docker-compose/launch_l1.bash
+++ docker-compose/launch_l1.bash
@@ -10,6 +10,4 @@ if [ "${ENV:-}" = "mainnet" ]; then
   exec anvil --fork-url http://l1geth-rpc.mainnet.scroll.tech:8545/l1 --chain-id 1 --host 0.0.0.0 --block-time 12 $EXTRA_PARAMS
 elif [ "${ENV:-}" = "sepolia" ]; then
   exec anvil --fork-url http://l1reth-rpc.sepolia.scroll.tech:8545 --chain-id 11155111 --host 0.0.0.0 --block-time 12 $EXTRA_PARAMS
-elif [ "${ENV:-}" = "dev" ]; then
-  exec anvil --chain-id 1 --host 0.0.0.0 --block-time 12
 fi
