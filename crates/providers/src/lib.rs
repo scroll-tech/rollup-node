fa4819878dc1e424562cc02672c81e796e47dac3
Gregory Edison <gregory.edison1993@gmail.com>
2025-07-16 09:42:29 +0200

    feat: rework blobl provider interface and add anvil blob provider

--- crates/providers/src/lib.rs
+++ crates/providers/src/lib.rs
@@ -4,24 +4,18 @@
 use alloy_provider::RootProvider;
 use scroll_alloy_network::Scroll;
 
-mod beacon;
-
-pub use beacon::{beacon_provider, BeaconProvider, OnlineBeaconClient};
-
 mod block;
 pub use block::BlockDataProvider;
 
 mod l1;
 pub use l1::{
-    blob::L1BlobProvider,
+    blob::{
+        AnvilBlobProvider, BlobProvider, BlobSource, ConsensusClientProvider, MockBeaconProvider,
+    },
     message::{DatabaseL1MessageProvider, L1MessageProvider},
     system_contract::{SystemContractProvider, AUTHORIZED_SIGNER_STORAGE_SLOT},
-    L1Provider, L1ProviderError, OnlineL1Provider,
+    FullL1Provider, L1Provider, L1ProviderError,
 };
-
-/// Test utils related to providers.
-#[cfg(any(test, feature = "test-utils"))]
-pub mod test_utils;
 
 /// An alias for a [`RootProvider`] using the [`Scroll`] network.
 pub type ScrollRootProvider = RootProvider<Scroll>;
