53b3d1cbdcd029f88cff03fee7dda84f2593776b
greg <82421016+greged93@users.noreply.github.com>
2025-07-28 14:53:52 +0200

    test: increase coverage (#218)
    * test: revert batch
    
    * test: derivation pipeline retry
    
    * test: derive blob batch
    
    * test: reorg e2e test
    
    * feat: force always_process_payload_attributes_on_canonical_head in node binary
    
    * fix: lints
    
    * merge diff
    
    ---------
    
    Co-authored-by: frisitano <francesco.risitano95@gmail.com>

--- crates/node/tests/e2e.rs
+++ crates/node/tests/e2e.rs
@@ -1,8 +1,8 @@
 //! End-to-end tests for the rollup node.
 
 use alloy_eips::BlockNumberOrTag;
-use alloy_primitives::{b256, Address, Bytes, Signature, U256};
-use futures::{task::noop_waker_ref, FutureExt, StreamExt};
+use alloy_primitives::{address, b256, Address, Bytes, Signature, B256, U256};
+use futures::StreamExt;
 use reth_network::{NetworkConfigBuilder, PeersInfo};
 use reth_rpc_api::EthApiServer;
 use reth_scroll_chainspec::SCROLL_DEV;
@@ -16,7 +16,7 @@ use rollup_node::{
     BeaconProviderArgs, DatabaseArgs, EngineDriverArgs, L1ProviderArgs,
     NetworkArgs as ScrollNetworkArgs, ScrollRollupNodeConfig, SequencerArgs,
 };
-use rollup_node_manager::{RollupManagerEvent, RollupManagerHandle};
+use rollup_node_manager::{RollupManagerCommand, RollupManagerEvent, RollupManagerHandle};
 use rollup_node_primitives::BatchCommitData;
 use rollup_node_providers::BlobSource;
 use rollup_node_sequencer::L1MessageInclusionMode;
@@ -24,12 +24,8 @@ use rollup_node_watcher::L1Notification;
 use scroll_alloy_consensus::TxL1Message;
 use scroll_network::{NewBlockWithPeer, SCROLL_MAINNET};
 use scroll_wire::{ScrollWireConfig, ScrollWireProtocolHandler};
-use std::{
-    path::PathBuf,
-    sync::Arc,
-    task::{Context, Poll},
-};
-use tokio::sync::Mutex;
+use std::{path::PathBuf, sync::Arc};
+use tokio::sync::{oneshot, Mutex};
 use tracing::trace;
 
 #[tokio::test]
@@ -471,7 +467,7 @@ async fn graceful_shutdown_consolidates_most_recent_batch_on_startup() -> eyre::
     );
 
     let (_, events) = ScrollWireProtocolHandler::new(ScrollWireConfig::new(true));
-    let (mut rnm, handle, l1_notification_tx) = config
+    let (rnm, handle, l1_notification_tx) = config
         .clone()
         .build(
             node.inner.network.clone(),
@@ -482,17 +478,13 @@ async fn graceful_shutdown_consolidates_most_recent_batch_on_startup() -> eyre::
         )
         .await?;
 
-    // Request an event stream from the rollup node manager and manually poll rnm to process the
-    // event stream request from the handle.
-    let mut rnm_events = Box::pin(handle.get_event_listener());
-    let mut rnm_events = loop {
-        let _ = rnm.poll_unpin(&mut Context::from_waker(noop_waker_ref()));
-        if let Poll::Ready(events) =
-            rnm_events.poll_unpin(&mut Context::from_waker(noop_waker_ref()))
-        {
-            break events.unwrap();
-        }
-    };
+    // Spawn a task that constantly polls the rnm to make progress.
+    let rnm_join_handle = tokio::spawn(async {
+        let _ = rnm.await;
+    });
+
+    // Request an event stream from the rollup node manager.
+    let mut rnm_events = handle.get_event_listener().await?;
 
     // Extract the L1 notification sender
     let l1_notification_tx = l1_notification_tx.unwrap();
@@ -526,8 +518,9 @@ async fn graceful_shutdown_consolidates_most_recent_batch_on_startup() -> eyre::
     let mut i = 1;
     loop {
         let block_info = loop {
-            let event = loop_until_event(&mut rnm, &mut rnm_events).await;
-            if let RollupManagerEvent::L1DerivedBlockConsolidated(consolidation_outcome) = event {
+            if let Some(RollupManagerEvent::L1DerivedBlockConsolidated(consolidation_outcome)) =
+                rnm_events.next().await
+            {
                 assert!(consolidation_outcome.block_info().block_info.number == i);
                 break consolidation_outcome.block_info().block_info;
             }
@@ -550,8 +543,9 @@ async fn graceful_shutdown_consolidates_most_recent_batch_on_startup() -> eyre::
     let mut i = 5;
     let hash = loop {
         let hash = loop {
-            let event = loop_until_event(&mut rnm, &mut rnm_events).await;
-            if let RollupManagerEvent::L1DerivedBlockConsolidated(consolidation_outcome) = event {
+            if let Some(RollupManagerEvent::L1DerivedBlockConsolidated(consolidation_outcome)) =
+                rnm_events.next().await
+            {
                 assert!(consolidation_outcome.block_info().block_info.number == i);
                 break consolidation_outcome.block_info().block_info.hash;
             }
@@ -574,13 +568,13 @@ async fn graceful_shutdown_consolidates_most_recent_batch_on_startup() -> eyre::
     assert_eq!(head_block_hash.header.hash, hash, "Head block hash does not match expected hash");
 
     // Simulate a shutdown of the rollup node manager by dropping it.
-    drop(rnm);
+    rnm_join_handle.abort();
     drop(l1_notification_tx);
     drop(rnm_events);
 
     // Start the RNM again.
     let (_, events) = ScrollWireProtocolHandler::new(ScrollWireConfig::new(true));
-    let (mut rnm, handle, l1_notification_tx) = config
+    let (rnm, handle, l1_notification_tx) = config
         .clone()
         .build(
             node.inner.network.clone(),
@@ -592,16 +586,13 @@ async fn graceful_shutdown_consolidates_most_recent_batch_on_startup() -> eyre::
         .await?;
     let l1_notification_tx = l1_notification_tx.unwrap();
 
-    // Get a handle to the event stream from the rollup node manager.
-    let mut rnm_events = Box::pin(handle.get_event_listener());
-    let mut rnm_events = loop {
-        let _ = rnm.poll_unpin(&mut Context::from_waker(noop_waker_ref()));
-        if let Poll::Ready(events) =
-            rnm_events.poll_unpin(&mut Context::from_waker(noop_waker_ref()))
-        {
-            break events.unwrap();
-        }
-    };
+    // Spawn a task that constantly polls the rnm to make progress.
+    tokio::spawn(async {
+        let _ = rnm.await;
+    });
+
+    // Request an event stream from the rollup node manager.
+    let mut rnm_events = handle.get_event_listener().await?;
 
     // Send the second batch again to mimic the watcher behaviour.
     l1_notification_tx.send(Arc::new(L1Notification::BatchCommit(batch_0_data.clone()))).await?;
@@ -609,8 +600,9 @@ async fn graceful_shutdown_consolidates_most_recent_batch_on_startup() -> eyre::
 
     // Lets fetch the first consolidated block event - this should be the first block of the batch.
     let l2_block = loop {
-        let event = loop_until_event(&mut rnm, &mut rnm_events).await;
-        if let RollupManagerEvent::L1DerivedBlockConsolidated(consolidation_outcome) = event {
+        if let Some(RollupManagerEvent::L1DerivedBlockConsolidated(consolidation_outcome)) =
+            rnm_events.next().await
+        {
             break consolidation_outcome.block_info().clone();
         }
     };
@@ -625,8 +617,9 @@ async fn graceful_shutdown_consolidates_most_recent_batch_on_startup() -> eyre::
     // commit.
     for i in 2..=57 {
         loop {
-            let event = loop_until_event(&mut rnm, &mut rnm_events).await;
-            if let RollupManagerEvent::L1DerivedBlockConsolidated(consolidation_outcome) = event {
+            if let Some(RollupManagerEvent::L1DerivedBlockConsolidated(consolidation_outcome)) =
+                rnm_events.next().await
+            {
                 assert!(consolidation_outcome.block_info().block_info.number == i);
                 break;
             }
@@ -649,25 +642,253 @@ async fn graceful_shutdown_consolidates_most_recent_batch_on_startup() -> eyre::
     Ok(())
 }
 
+#[tokio::test]
+async fn can_handle_batch_revert() -> eyre::Result<()> {
+    reth_tracing::init_test_tracing();
+    let chain_spec = (*SCROLL_MAINNET).clone();
+
+    // Launch a node
+    let (mut nodes, _tasks, _) =
+        setup_engine(default_test_scroll_rollup_node_config(), 1, chain_spec.clone(), false, false)
+            .await?;
+    let node = nodes.pop().unwrap();
+
+    // Instantiate the rollup node manager.
+    let mut config = default_test_scroll_rollup_node_config();
+    let path = node.inner.config.datadir().db().join("scroll.db?mode=rwc");
+    let path = PathBuf::from("sqlite://".to_string() + &*path.to_string_lossy());
+    config.database_args.path = Some(path.clone());
+    config.beacon_provider_args.url = Some(
+        "http://dummy:8545"
+            .parse()
+            .expect("valid url that will not be used as test batches use calldata"),
+    );
+
+    let (_, events) = ScrollWireProtocolHandler::new(ScrollWireConfig::new(true));
+    let (rnm, handle, l1_watcher_tx) = config
+        .clone()
+        .build(
+            node.inner.network.clone(),
+            events,
+            node.inner.add_ons_handle.rpc_handle.rpc_server_handles.clone(),
+            chain_spec.clone(),
+            path.clone(),
+        )
+        .await?;
+    let l1_watcher_tx = l1_watcher_tx.unwrap();
+
+    // Spawn a task that constantly polls the rnm to make progress.
+    tokio::spawn(async {
+        let _ = rnm.await;
+    });
+
+    // Request an event stream from the rollup node manager and manually poll rnm to process the
+    // event stream request from the handle.
+    let mut rnm_events = handle.get_event_listener().await?;
+
+    // Load test batches
+    let raw_calldata_0 = read_to_bytes("./tests/testdata/batch_0_calldata.bin")?;
+    let batch_0_data = BatchCommitData {
+        hash: b256!("5AAEB6101A47FC16866E80D77FFE090B6A7B3CF7D988BE981646AB6AEDFA2C42"),
+        index: 1,
+        block_number: 18318207,
+        block_timestamp: 1696935971,
+        calldata: Arc::new(raw_calldata_0),
+        blob_versioned_hash: None,
+        finalized_block_number: None,
+    };
+    let raw_calldata_1 = read_to_bytes("./tests/testdata/batch_1_calldata.bin")?;
+    let batch_1_data = BatchCommitData {
+        hash: b256!("AA8181F04F8E305328A6117FA6BC13FA2093A3C4C990C5281DF95A1CB85CA18F"),
+        index: 2,
+        block_number: 18318215,
+        block_timestamp: 1696936000,
+        calldata: Arc::new(raw_calldata_1),
+        blob_versioned_hash: None,
+        finalized_block_number: None,
+    };
+    let revert_batch_data = BatchCommitData {
+        hash: B256::random(),
+        index: 2,
+        block_number: 18318220,
+        block_timestamp: 1696936500,
+        calldata: Arc::new(Default::default()),
+        blob_versioned_hash: None,
+        finalized_block_number: None,
+    };
+
+    // Send the first batch.
+    l1_watcher_tx.send(Arc::new(L1Notification::BatchCommit(batch_0_data))).await?;
+
+    // Read the first 4 blocks.
+    loop {
+        if let Some(RollupManagerEvent::L1DerivedBlockConsolidated(consolidation_outcome)) =
+            rnm_events.next().await
+        {
+            if consolidation_outcome.block_info().block_info.number == 4 {
+                break
+            }
+        }
+    }
+
+    // Send the second batch.
+    l1_watcher_tx.send(Arc::new(L1Notification::BatchCommit(batch_1_data))).await?;
+
+    // Read the next 42 blocks.
+    loop {
+        if let Some(RollupManagerEvent::L1DerivedBlockConsolidated(consolidation_outcome)) =
+            rnm_events.next().await
+        {
+            if consolidation_outcome.block_info().block_info.number == 46 {
+                break
+            }
+        }
+    }
+
+    let (tx, rx) = oneshot::channel();
+    handle.send_command(RollupManagerCommand::Status(tx)).await;
+
+    let status = rx.await?;
+
+    // Assert the forkchoice state is above 4
+    assert!(status.forkchoice_state.head_block_info().number > 4);
+    assert!(status.forkchoice_state.safe_block_info().number > 4);
+
+    // Send the third batch which should trigger the revert.
+    l1_watcher_tx.send(Arc::new(L1Notification::BatchCommit(revert_batch_data))).await?;
+
+    // Wait for the third batch to be proceeded.
+    tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
+
+    let (tx, rx) = oneshot::channel();
+    handle.send_command(RollupManagerCommand::Status(tx)).await;
+
+    let status = rx.await?;
+
+    // Assert the forkchoice state was reset to 4.
+    assert_eq!(status.forkchoice_state.head_block_info().number, 4);
+    assert_eq!(status.forkchoice_state.safe_block_info().number, 4);
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn can_handle_reorgs_while_sequencing() -> eyre::Result<()> {
+    reth_tracing::init_test_tracing();
+    let chain_spec = (*SCROLL_DEV).clone();
+
+    // Launch a node
+    let mut config = default_test_scroll_rollup_node_config();
+    config.sequencer_args.block_time = 0;
+    let (mut nodes, _tasks, _) = setup_engine(config, 1, chain_spec.clone(), false, false).await?;
+    let node = nodes.pop().unwrap();
+
+    // Instantiate the rollup node manager.
+    let mut config = default_sequencer_test_scroll_rollup_node_config();
+    let path = node.inner.config.datadir().db().join("scroll.db?mode=rwc");
+    let path = PathBuf::from("sqlite://".to_string() + &*path.to_string_lossy());
+    config.database_args.path = Some(path.clone());
+    config.beacon_provider_args.url = Some(
+        "http://dummy:8545"
+            .parse()
+            .expect("valid url that will not be used as test batches use calldata"),
+    );
+    config.engine_driver_args.sync_at_startup = false;
+    config.sequencer_args.max_l1_messages_per_block = 1;
+
+    let (_, events) = ScrollWireProtocolHandler::new(ScrollWireConfig::new(true));
+    let (rnm, handle, l1_watcher_tx) = config
+        .clone()
+        .build(
+            node.inner.network.clone(),
+            events,
+            node.inner.add_ons_handle.rpc_handle.rpc_server_handles.clone(),
+            chain_spec.clone(),
+            path.clone(),
+        )
+        .await?;
+    let l1_watcher_tx = l1_watcher_tx.unwrap();
+
+    // Spawn a task that constantly polls the rnm to make progress.
+    tokio::spawn(async {
+        let _ = rnm.await;
+    });
+
+    // Request an event stream from the rollup node manager and manually poll rnm to process the
+    // event stream request from the handle.
+    let mut rnm_events = handle.get_event_listener().await?;
+
+    // Send an L1 message.
+    let message = TxL1Message {
+        queue_index: 0,
+        gas_limit: 21000,
+        to: Default::default(),
+        value: Default::default(),
+        sender: address!("f39Fd6e51aad88F6F4ce6aB8827279cffFb92266"),
+        input: Default::default(),
+    };
+
+    // Let the sequencer build 10 blocks before performing the reorg process.
+    let mut i = 0;
+    loop {
+        handle.build_block().await;
+        if let Some(RollupManagerEvent::BlockSequenced(_)) = rnm_events.next().await {
+            if i == 10 {
+                break
+            }
+            i += 1;
+        }
+    }
+
+    // Send a L1 message and wait for it to be indexed.
+    l1_watcher_tx
+        .send(Arc::new(L1Notification::L1Message { message, block_number: 10, block_timestamp: 0 }))
+        .await?;
+    loop {
+        if let Some(RollupManagerEvent::L1MessageIndexed(index)) = rnm_events.next().await {
+            assert_eq!(index, 0);
+            break
+        }
+    }
+    l1_watcher_tx.send(Arc::new(L1Notification::NewBlock(10))).await?;
+
+    // Wait for block that contains the L1 message.
+    handle.build_block().await;
+    let l2_reorged_height;
+    loop {
+        if let Some(RollupManagerEvent::BlockSequenced(block)) = rnm_events.next().await {
+            if block.body.transactions.iter().any(|tx| tx.is_l1_message()) {
+                l2_reorged_height = block.header.number;
+                break
+            }
+        }
+    }
+
+    // Issue and wait for the reorg.
+    l1_watcher_tx.send(Arc::new(L1Notification::Reorg(9))).await?;
+    loop {
+        if let Some(RollupManagerEvent::Reorg(height)) = rnm_events.next().await {
+            assert_eq!(height, 9);
+            break
+        }
+    }
+
+    // Get the next sequenced L2 block.
+    handle.build_block().await;
+    loop {
+        if let Some(RollupManagerEvent::BlockSequenced(block)) = rnm_events.next().await {
+            assert_eq!(block.number, l2_reorged_height);
+            break
+        }
+    }
+
+    Ok(())
+}
+
 /// Read the file provided at `path` as a [`Bytes`].
 pub fn read_to_bytes<P: AsRef<std::path::Path>>(path: P) -> eyre::Result<Bytes> {
     use std::str::FromStr;
     Ok(Bytes::from_str(&std::fs::read_to_string(path)?)?)
-}
-
-async fn loop_until_event(
-    rnm: &mut (impl futures::Future<Output = ()> + Unpin),
-    rnm_events: &mut (impl futures::Stream<Item = RollupManagerEvent> + Unpin),
-) -> RollupManagerEvent {
-    loop {
-        let _ = rnm.poll_unpin(&mut Context::from_waker(noop_waker_ref()));
-        if let Poll::Ready(Some(event)) =
-            rnm_events.poll_next_unpin(&mut Context::from_waker(noop_waker_ref()))
-        {
-            return event;
-        }
-        tokio::task::yield_now().await;
-    }
 }
 
 /// Waits for n events to be emitted.
