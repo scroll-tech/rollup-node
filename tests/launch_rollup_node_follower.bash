a9f1127f92d11753dec553962be0bf90ad1470bc
colinlyguo <colinlyguo@scroll.io>
2025-07-31 23:43:14 +0800

    feat: integrate l2geth in docker-compose e2e tests

--- tests/launch_rollup_node_follower.bash
+++ tests/launch_rollup_node_follower.bash
@@ -5,7 +5,6 @@ exec rollup-node node --chain dev --datadir=/l2reth --metrics=0.0.0.0:6060 --net
   --http --http.addr=0.0.0.0 --http.port=8545 --http.corsdomain "*" --http.api admin,debug,eth,net,trace,txpool,web3,rpc,reth,ots,flashbots,miner,mev \
   --ws --ws.addr=0.0.0.0 --ws.port=8546 --ws.api admin,debug,eth,net,trace,txpool,web3,rpc,reth,ots,flashbots,miner,mev \
   --log.stdout.format log-fmt -vvv \
-  --test \
   --txpool.pending-max-count=1000 \
   --builder.gaslimit=20000000 \
   --rpc.max-connections=5000 \
