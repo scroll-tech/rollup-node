13a7364629ada124046ea447bd43ba4b8ce7b7a4
colinlyguo <colinlyguo@scroll.io>
2025-08-15 14:46:02 +0800

    increase log level

--- tests/launch_l2geth.bash
+++ tests/launch_l2geth.bash
@@ -13,7 +13,7 @@ exec geth --datadir=/l2geth \
   --port 30303 --syncmode full --networkid 1337 --nodiscover \
   --http --http.addr 0.0.0.0 --http.port 8545 --http.vhosts "*" --http.corsdomain "*" --http.api "eth,scroll,net,web3,debug" \
   --ws --ws.addr 0.0.0.0 --ws.port 8546 --ws.api "eth,scroll,net,web3,debug" \
-  --pprof --pprof.addr 0.0.0.0 --pprof.port 6060 --metrics --verbosity 3 \
+  --pprof --pprof.addr 0.0.0.0 --pprof.port 6060 --metrics --verbosity 5 --log.debug \
   --l1.endpoint "http://l1-node:8545" --l1.confirmations finalized --l1.sync.startblock 0 \
   --gcmode archive --cache.noprefetch --cache.snapshot=0 --snapshot=false \
   --nat extip:0.0.0.0
