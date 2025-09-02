43e59ce03dc70f66906e7bc9619d07e5949072e1
Morty <yiweichi1@gmail.com>
2025-09-02 15:59:31 +0800

    fix: broadcast block with new signature encode

--- tests/launch_l2geth.bash
+++ tests/launch_l2geth.bash
@@ -5,14 +5,14 @@ set -e
 
 # Create config.toml with static nodes instead of bootnodes
 echo '[Node.P2P]' > ./l2geth/config.toml
-echo 'StaticNodes = ["enode://3983278a7cab48862d9ab3187278edf376a0736a7deb55472a5650592f6922ce626a1ea7d74b77b9a679694b343f5e93ea97d5d60a9db4e4b51bb0c23a36d01b@localhost:30303"]' >> ./l2geth/config.toml
+echo 'StaticNodes = ["enode://3983278a7cab48862d9ab3187278edf376a0736a7deb55472a5650592f6922ce626a1ea7d74b77b9a679694b343f5e93ea97d5d60a9db4e4b51bb0c23a36d01b@127.0.0.1:30303"]' >> ./l2geth/config.toml
 
 echo "Starting l2geth as follower..."
 exec ./geth --datadir=./l2geth \
   --config ./l2geth/config.toml \
   --port 30304 --syncmode full --networkid 1337 --nodiscover \
-  --http --http.addr 0.0.0.0 --http.port 8545 --http.vhosts "*" --http.corsdomain "*" --http.api "eth,scroll,net,web3,debug" \
-  --ws --ws.addr 0.0.0.0 --ws.port 8546 --ws.api "eth,scroll,net,web3,debug" \
+  --http --http.addr 0.0.0.0 --http.port 8549 --http.vhosts "*" --http.corsdomain "*" --http.api "eth,scroll,net,web3,debug" \
+  --ws --ws.addr 0.0.0.0 --ws.port 8548 --ws.api "eth,scroll,net,web3,debug" \
   --pprof --pprof.addr 0.0.0.0 --pprof.port 6060 --metrics --verbosity 5 --log.debug \
   --l1.endpoint "http://localhost:8544" --l1.confirmations finalized --l1.sync.startblock 0 \
   --gcmode archive --cache.noprefetch --cache.snapshot=0 --snapshot=false \
