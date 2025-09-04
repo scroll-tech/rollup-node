cec4abde22bd5213cd0f156671d60f8dd4c91ddc
frisitano <francesco.risitano95@gmail.com>
2025-09-04 20:51:28 +0800

    fix tests

--- crates/chain-orchestrator/src/lib.rs
+++ crates/chain-orchestrator/src/lib.rs
@@ -1170,24 +1170,28 @@ mod test {
         rand::rng().fill(bytes.as_mut_slice());
         let mut u = Unstructured::new(&bytes);
 
-        let batch_commit = BatchCommitData::arbitrary(&mut u).unwrap();
-        chain_orchestrator
-            .handle_l1_notification(L1Notification::BatchCommit(batch_commit.clone()));
+        // Insert a batch commit in the database to satisfy the chain orchestrator consistency
+        // checks
+        let batch_0 = BatchCommitData { index: 0, ..Arbitrary::arbitrary(&mut u).unwrap() };
+        db.insert_batch(batch_0).await.unwrap();
+
+        let batch_1 = BatchCommitData { index: 1, ..Arbitrary::arbitrary(&mut u).unwrap() };
+        chain_orchestrator.handle_l1_notification(L1Notification::BatchCommit(batch_1.clone()));
 
         let event = chain_orchestrator.next().await.unwrap().unwrap();
 
         // Verify the event structure
         match event {
             ChainOrchestratorEvent::BatchCommitIndexed { batch_info, safe_head, .. } => {
-                assert_eq!(batch_info.index, batch_commit.index);
-                assert_eq!(batch_info.hash, batch_commit.hash);
+                assert_eq!(batch_info.index, batch_1.index);
+                assert_eq!(batch_info.hash, batch_1.hash);
                 assert_eq!(safe_head, None); // No safe head since no batch revert
             }
             _ => panic!("Expected BatchCommitIndexed event"),
         }
 
-        let batch_commit_result = db.get_batch_by_index(batch_commit.index).await.unwrap().unwrap();
-        assert_eq!(batch_commit, batch_commit_result);
+        let batch_commit_result = db.get_batch_by_index(batch_1.index).await.unwrap().unwrap();
+        assert_eq!(batch_1, batch_commit_result);
     }
 
     #[tokio::test]
@@ -1199,6 +1203,15 @@ mod test {
         let mut bytes = [0u8; 1024];
         rand::rng().fill(bytes.as_mut_slice());
         let mut u = Unstructured::new(&bytes);
+
+        // Insert batch 0 into the database to satisfy the consistency conditions in the chain
+        // orchestrator
+        let batch_0 = BatchCommitData {
+            index: 99,
+            calldata: Arc::new(vec![].into()),
+            ..Arbitrary::arbitrary(&mut u).unwrap()
+        };
+        db.insert_batch(batch_0).await.unwrap();
 
         // Create sequential batches
         let batch_1 = BatchCommitData {
@@ -1323,13 +1336,24 @@ mod test {
         rand::rng().fill(bytes.as_mut_slice());
         let mut u = Unstructured::new(&bytes);
 
-        let message = TxL1Message {
+        // Insert an initial message in the database to satisfy the consistency checks in the chain
+        // orchestrator.
+        let message_0 = L1MessageEnvelope {
+            transaction: TxL1Message {
+                queue_index: TEST_L1_MESSAGE_QUEUE_INDEX_BOUNDARY - 2,
+                ..Arbitrary::arbitrary(&mut u).unwrap()
+            },
+            ..Arbitrary::arbitrary(&mut u).unwrap()
+        };
+        db.insert_l1_message(message_0).await.unwrap();
+
+        let message_1 = TxL1Message {
             queue_index: TEST_L1_MESSAGE_QUEUE_INDEX_BOUNDARY - 1,
             ..Arbitrary::arbitrary(&mut u).unwrap()
         };
         let block_number = u64::arbitrary(&mut u).unwrap();
         chain_orchestrator.handle_l1_notification(L1Notification::L1Message {
-            message: message.clone(),
+            message: message_1.clone(),
             block_number,
             block_timestamp: 0,
         });
@@ -1337,8 +1361,8 @@ mod test {
         let _ = chain_orchestrator.next().await;
 
         let l1_message_result =
-            db.get_l1_message_by_index(message.queue_index).await.unwrap().unwrap();
-        let l1_message = L1MessageEnvelope::new(message, block_number, None, None);
+            db.get_l1_message_by_index(message_1.queue_index).await.unwrap().unwrap();
+        let l1_message = L1MessageEnvelope::new(message_1, block_number, None, None);
 
         assert_eq!(l1_message, l1_message_result);
     }
@@ -1395,29 +1419,45 @@ mod test {
         rand::rng().fill(bytes.as_mut_slice());
         let mut u = Unstructured::new(&bytes);
 
+        // Insert batch 0 into the database to satisfy the consistency checks in the chain
+        // orchestrator
+        let batch_0 =
+            BatchCommitData { index: 0, block_number: 0, ..Arbitrary::arbitrary(&mut u).unwrap() };
+        db.insert_batch(batch_0).await.unwrap();
+
+        // Insert l1 message into the database to satisfy the consistency checks in the chain
+        // orchestrator
+        let l1_message = L1MessageEnvelope {
+            queue_hash: None,
+            l1_block_number: 0,
+            l2_block_number: None,
+            transaction: TxL1Message { queue_index: 0, ..Arbitrary::arbitrary(&mut u).unwrap() },
+        };
+        db.insert_l1_message(l1_message).await.unwrap();
+
         // Generate a 3 random batch inputs and set their block numbers
         let mut batch_commit_block_1 = BatchCommitData::arbitrary(&mut u).unwrap();
         batch_commit_block_1.block_number = 1;
         batch_commit_block_1.index = 1;
         let batch_commit_block_1 = batch_commit_block_1;
 
-        let mut batch_commit_block_20 = BatchCommitData::arbitrary(&mut u).unwrap();
-        batch_commit_block_20.block_number = 20;
-        batch_commit_block_20.index = 20;
-        let batch_commit_block_20 = batch_commit_block_20;
+        let mut batch_commit_block_2 = BatchCommitData::arbitrary(&mut u).unwrap();
+        batch_commit_block_2.block_number = 2;
+        batch_commit_block_2.index = 2;
+        let batch_commit_block_2 = batch_commit_block_2;
 
-        let mut batch_commit_block_30 = BatchCommitData::arbitrary(&mut u).unwrap();
-        batch_commit_block_30.block_number = 30;
-        batch_commit_block_30.index = 30;
-        let batch_commit_block_30 = batch_commit_block_30;
+        let mut batch_commit_block_3 = BatchCommitData::arbitrary(&mut u).unwrap();
+        batch_commit_block_3.block_number = 3;
+        batch_commit_block_3.index = 3;
+        let batch_commit_block_3 = batch_commit_block_3;
 
         // Index batch inputs
         chain_orchestrator
             .handle_l1_notification(L1Notification::BatchCommit(batch_commit_block_1.clone()));
         chain_orchestrator
-            .handle_l1_notification(L1Notification::BatchCommit(batch_commit_block_20.clone()));
+            .handle_l1_notification(L1Notification::BatchCommit(batch_commit_block_2.clone()));
         chain_orchestrator
-            .handle_l1_notification(L1Notification::BatchCommit(batch_commit_block_30.clone()));
+            .handle_l1_notification(L1Notification::BatchCommit(batch_commit_block_3.clone()));
 
         // Generate 3 random L1 messages and set their block numbers
         let l1_message_block_1 = L1MessageEnvelope {
@@ -1426,15 +1466,15 @@ mod test {
             l2_block_number: None,
             transaction: TxL1Message { queue_index: 1, ..Arbitrary::arbitrary(&mut u).unwrap() },
         };
-        let l1_message_block_20 = L1MessageEnvelope {
+        let l1_message_block_2 = L1MessageEnvelope {
             queue_hash: None,
-            l1_block_number: 20,
+            l1_block_number: 2,
             l2_block_number: None,
             transaction: TxL1Message { queue_index: 2, ..Arbitrary::arbitrary(&mut u).unwrap() },
         };
-        let l1_message_block_30 = L1MessageEnvelope {
+        let l1_message_block_3 = L1MessageEnvelope {
             queue_hash: None,
-            l1_block_number: 30,
+            l1_block_number: 3,
             l2_block_number: None,
             transaction: TxL1Message { queue_index: 3, ..Arbitrary::arbitrary(&mut u).unwrap() },
         };
@@ -1446,18 +1486,18 @@ mod test {
             block_timestamp: 0,
         });
         chain_orchestrator.handle_l1_notification(L1Notification::L1Message {
-            message: l1_message_block_20.clone().transaction,
-            block_number: l1_message_block_20.clone().l1_block_number,
+            message: l1_message_block_2.clone().transaction,
+            block_number: l1_message_block_2.clone().l1_block_number,
             block_timestamp: 0,
         });
         chain_orchestrator.handle_l1_notification(L1Notification::L1Message {
-            message: l1_message_block_30.clone().transaction,
-            block_number: l1_message_block_30.clone().l1_block_number,
+            message: l1_message_block_3.clone().transaction,
+            block_number: l1_message_block_3.clone().l1_block_number,
             block_timestamp: 0,
         });
 
-        // Reorg at block 20
-        chain_orchestrator.handle_l1_notification(L1Notification::Reorg(20));
+        // Reorg at block 2
+        chain_orchestrator.handle_l1_notification(L1Notification::Reorg(2));
 
         for _ in 0..7 {
             chain_orchestrator.next().await.unwrap().unwrap();
@@ -1469,7 +1509,7 @@ mod test {
 
         assert_eq!(3, batch_commits.len());
         assert!(batch_commits.contains(&batch_commit_block_1));
-        assert!(batch_commits.contains(&batch_commit_block_20));
+        assert!(batch_commits.contains(&batch_commit_block_2));
 
         // check that the L1 message at block 30 is deleted
         let l1_messages = db
@@ -1479,9 +1519,9 @@ mod test {
             .map(|res| res.unwrap())
             .collect::<Vec<_>>()
             .await;
-        assert_eq!(2, l1_messages.len());
+        assert_eq!(3, l1_messages.len());
         assert!(l1_messages.contains(&l1_message_block_1));
-        assert!(l1_messages.contains(&l1_message_block_20));
+        assert!(l1_messages.contains(&l1_message_block_2));
     }
 
     // We ignore this test for now as it requires a more complex setup which leverages an L2 node
