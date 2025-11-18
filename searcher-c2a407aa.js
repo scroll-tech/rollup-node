3388d479e530196ab1aa6057a670cb83c74b603e
frisitano <frisitano@users.noreply.github.com>
2025-11-18 05:52:01 +0000

    deploy: 655baa77f107edbee508b4bfb3e16c0a8e4c88d4

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-52ccf182.js',
+                path_to_root + 'searchindex-ab3e4552.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
