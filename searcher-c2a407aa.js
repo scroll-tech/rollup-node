38f0dd82e5cc283dd56b624b07d8601765f0281e
yiweichi <yiweichi@users.noreply.github.com>
2025-12-05 11:25:03 +0000

    deploy: 8c1f7d15e7e3673f7bb4d06cdef5a02c78f84d6f

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-ab3e4552.js',
+                path_to_root + 'searchindex-6dee36f6.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
