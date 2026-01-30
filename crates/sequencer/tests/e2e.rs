dab4f8422fde0b68a860137e0eb8d039778eb218
frisitano <35734660+frisitano@users.noreply.github.com>
2026-01-30 15:33:20 +0400

    introduce remote block source add on (#484)
    * introduce remote block source add on
    
    * add docker compose tests
    
    * lint

--- crates/sequencer/tests/e2e.rs
+++ crates/sequencer/tests/e2e.rs
@@ -10,8 +10,8 @@ use rollup_node::{
     constants::SCROLL_GAS_LIMIT,
     test_utils::{default_test_scroll_rollup_node_config, setup_engine},
     BlobProviderArgs, ChainOrchestratorArgs, ConsensusArgs, EngineDriverArgs, L1ProviderArgs,
-    PprofArgs, RollupNodeDatabaseArgs, RollupNodeGasPriceOracleArgs, RollupNodeNetworkArgs,
-    RpcArgs, ScrollRollupNodeConfig, SequencerArgs, SignerArgs,
+    PprofArgs, RemoteBlockSourceArgs, RollupNodeDatabaseArgs, RollupNodeGasPriceOracleArgs,
+    RollupNodeNetworkArgs, RpcArgs, ScrollRollupNodeConfig, SequencerArgs, SignerArgs,
 };
 use rollup_node_chain_orchestrator::ChainOrchestratorEvent;
 use rollup_node_primitives::{sig_encode_hash, BlockInfo, L1MessageEnvelope};
@@ -212,7 +212,7 @@ async fn can_build_blocks_with_delayed_l1_messages() {
 
     // setup a test node
     let (mut nodes, _tasks, wallet) =
-        setup_engine(default_test_scroll_rollup_node_config(), 1, chain_spec, false, false)
+        setup_engine(default_test_scroll_rollup_node_config(), 1, chain_spec, false, false, None)
             .await
             .unwrap();
 
@@ -337,7 +337,7 @@ async fn can_build_blocks_with_finalized_l1_messages() {
     let chain_spec = SCROLL_DEV.clone();
     // setup a test node
     let (mut nodes, _tasks, wallet) =
-        setup_engine(default_test_scroll_rollup_node_config(), 1, chain_spec, false, false)
+        setup_engine(default_test_scroll_rollup_node_config(), 1, chain_spec, false, false, None)
             .await
             .unwrap();
     let node = nodes.pop().unwrap();
@@ -509,10 +509,11 @@ async fn can_sequence_blocks_with_private_key_file() -> eyre::Result<()> {
         database: None,
         rpc_args: RpcArgs::default(),
         pprof_args: PprofArgs::default(),
+        remote_block_source_args: RemoteBlockSourceArgs::default(),
     };
 
     let (nodes, _tasks, wallet) =
-        setup_engine(rollup_manager_args, 1, chain_spec, false, false).await?;
+        setup_engine(rollup_manager_args, 1, chain_spec, false, false, None).await?;
     let wallet = Arc::new(Mutex::new(wallet));
 
     let sequencer_rnm_handle = nodes[0].inner.add_ons_handle.rollup_manager_handle.clone();
@@ -611,10 +612,11 @@ async fn can_sequence_blocks_with_hex_key_file_without_prefix() -> eyre::Result<
         database: None,
         rpc_args: RpcArgs::default(),
         pprof_args: PprofArgs::default(),
+        remote_block_source_args: RemoteBlockSourceArgs::default(),
     };
 
     let (nodes, _tasks, wallet) =
-        setup_engine(rollup_manager_args, 1, chain_spec, false, false).await?;
+        setup_engine(rollup_manager_args, 1, chain_spec, false, false, None).await?;
     let wallet = Arc::new(Mutex::new(wallet));
 
     let sequencer_rnm_handle = nodes[0].inner.add_ons_handle.rollup_manager_handle.clone();
@@ -684,6 +686,7 @@ async fn can_build_blocks_and_exit_at_gas_limit() {
         chain_spec,
         false,
         false,
+        None,
     )
     .await
     .unwrap();
@@ -770,6 +773,7 @@ async fn can_build_blocks_and_exit_at_time_limit() {
         chain_spec,
         false,
         false,
+        None,
     )
     .await
     .unwrap();
@@ -850,7 +854,7 @@ async fn should_limit_l1_message_cumulative_gas() {
     // setup a test node
     let chain_spec = SCROLL_DEV.clone();
     let (mut nodes, _tasks, wallet) =
-        setup_engine(default_test_scroll_rollup_node_config(), 1, chain_spec, false, false)
+        setup_engine(default_test_scroll_rollup_node_config(), 1, chain_spec, false, false, None)
             .await
             .unwrap();
     let node = nodes.pop().unwrap();
@@ -967,7 +971,7 @@ async fn should_not_add_skipped_messages() {
     // setup a test node
     let chain_spec = SCROLL_DEV.clone();
     let (mut nodes, _tasks, wallet) =
-        setup_engine(default_test_scroll_rollup_node_config(), 1, chain_spec, false, false)
+        setup_engine(default_test_scroll_rollup_node_config(), 1, chain_spec, false, false, None)
             .await
             .unwrap();
     let node = nodes.pop().unwrap();
